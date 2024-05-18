38fe68aba37ed4d1d4dd278de150322312c06537
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-05-18 14:39:07 +0200

    Set up

---
+++ src/Crowdfund.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+/**
+ * @title Crowdfund Contract
+ * @author Kelly Smulian
+ * @notice This contract is for creating a crowdfund program
+ * @dev
+ */
+contract Crowdfund {}
