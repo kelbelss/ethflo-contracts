8c7a0473dc0af104156d6415b6b8d53d224a898d
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-06-03 19:22:07 +0200

    Work on tests

--- test/Crowdfund.t.sol
+++ test/Crowdfund.t.sol
@@ -4,4 +4,12 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 import {Crowdfund} from "../src/Crowdfund.sol";
 
-contract CrowdfundTest is Test {}
+contract CrowdfundTest is Test {
+    Crowdfund crowdfund;
+
+    address public CREATOR = makeAddr("creator");
+    uint256 constant GOAL = 1 ether;
+    uint256 constant DURATION = 0; // figure out times
+
+    function testCreateFundraiser() public {}
+}
