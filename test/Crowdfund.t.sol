6a73bea94d99f99a1c1c4eab36bfb8a41f8ab8db
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-06-03 19:49:36 +0200

    Test create fundraiser test progress

--- test/Crowdfund.t.sol
+++ test/Crowdfund.t.sol
@@ -8,8 +8,13 @@ contract CrowdfundTest is Test {
     Crowdfund crowdfund;
 
     address public CREATOR = makeAddr("creator");
-    uint256 constant GOAL = 1 ether;
+    string constant NAME = "TesterFundraiser";
     uint256 constant DURATION = 0; // figure out times
+    uint256 constant GOAL = 1 ether;
 
-    function testCreateFundraiser() public {}
+    function testCreateFundraiser() public {
+        vm.startPrank(CREATOR);
+        crowdfund.createFundraiser(NAME, DURATION, GOAL);
+        assertEq(crowdfund.createFundraiser(NAME, DURATION, GOAL));
+    }
 }
