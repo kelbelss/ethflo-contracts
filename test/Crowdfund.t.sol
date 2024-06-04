f06164a768972c23db0b27359d2b215ab7210187
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-06-04 16:25:00 +0200

    Fix test

--- test/Crowdfund.t.sol
+++ test/Crowdfund.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "lib/forge-std/src/Test.sol";
 import {Crowdfund} from "../src/Crowdfund.sol";
 
 contract CrowdfundTest is Test {
@@ -9,12 +9,15 @@ contract CrowdfundTest is Test {
 
     address public CREATOR = makeAddr("creator");
     string constant NAME = "TesterFundraiser";
-    uint256 constant DURATION = 0; // figure out times
+    uint256 constant DEADLINE = 0; // figure out times
     uint256 constant GOAL = 1 ether;
+
+    function setUp() public {
+        crowdfund = new Crowdfund();
+    }
 
     function testCreateFundraiser() public {
         vm.startPrank(CREATOR);
-        crowdfund.createFundraiser(NAME, DURATION, GOAL);
-        assertEq(crowdfund.createFundraiser(NAME, DURATION, GOAL));
+        crowdfund.createFundraiser(CREATOR, NAME, DEADLINE, GOAL);
     }
 }
