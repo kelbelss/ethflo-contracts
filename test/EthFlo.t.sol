41a467856248e24a494530a80fa2d022134663e1
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-06-10 11:17:18 +0200

    Correct Errors

--- test/EthFlo.t.sol
+++ test/EthFlo.t.sol
@@ -15,23 +15,25 @@ contract EthFloTest is Test {
         ethFlo = new EthFlo();
     }
 
+    // createFundraiser TESTS
+
     function test_createFundraiser_success() public {
         vm.startPrank(CREATOR);
         uint256 id = ethFlo.createFundraiser({_creatorAddr: CREATOR, _deadline: 6 days, _goal: 50});
 
-        // TODO check variables were set correctly
+        // check variables were set correctly
 
         (address _creator, uint256 _deadline, uint256 _goal) = ethFlo.fundraisers(id);
 
         assertEq(_creator, CREATOR, "Creator not set correctly");
-        assertEq(_deadline, 5 days, "Deadline not set correctly");
-        // TODO complete
+        assertEq(_deadline, 6 days, "Deadline not set correctly");
+        assertEq(_goal, 50, "Goal not set correctly");
     }
 
     function test_event_createFundraiser_success() public {
         vm.expectEmit(true, false, false, true);
-        emit EthFlo.CreateFundraiser(address(CREATOR), 6 days, 50);
-        ethFlo.createFundraiser(address(CREATOR), 6 days, 50);
+        emit EthFlo.CreateFundraiser(CREATOR, 6 days, 50);
+        ethFlo.createFundraiser(CREATOR, 6 days, 50);
     }
 
     function test_createFundraiser_fail_DeadlineError() public {
