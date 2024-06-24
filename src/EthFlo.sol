d8c819046344ef0a13a13681e4cc0eed49bfa328
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-06-24 15:28:28 +0200

    Fix remappings

--- src/EthFlo.sol
+++ src/EthFlo.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.22;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@aave/contracts/interfaces/IPool.sol";
+import {IPool} from "@aave/contracts/interfaces/IPool.sol";
 
 /**
  * @title EthFlo Contract
